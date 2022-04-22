791e4f9e6a5115ca870d577dfbbfa98250fb1461
rohitsejwal21 <87788776+rohitsejwal21@users.noreply.github.com>
2022-04-22 23:42:57 +0530

    KNN implementation

--- KNN.py
+++ KNN.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 def euclidean_distance(x1, x2):
-    #Euclidean distance between two points
     
     return np.sqrt(np.sum((x1-x2)**2))
 
