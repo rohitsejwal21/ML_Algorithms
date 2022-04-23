2a381073e7899279d2879d853d71a1ba42940aca
rohitsejwal21 <87788776+rohitsejwal21@users.noreply.github.com>
2022-04-23 22:28:01 +0530

    Linear Regression

--- LinearRegression.py
+++ LinearRegression.py
@@ -14,11 +14,11 @@ class LinearRegression:
     def fit(self, X, y):
         n_samples, n_features = X.shape
 
-        #1. Init weights and bias parameters
+        # Init weights and bias parameters
         self.weights = np.zeros(n_features)
         self.bias = 0
 
-        #2. Gradient Descent
+        # Gradient Descent
         for _ in range(self.n_iters):
 
             y_predicted = np.dot(X, self.weights) + self.bias
